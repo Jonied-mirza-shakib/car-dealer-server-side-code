fa53f83c0f3847d78fa94f0efa47112fde4b043f
Jonied-mirza-shakib <MDSHAKIB01088@gmail.com>
2022-04-29 11:28:56 +0600

    server side basic setup

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+let cors = require('cors')
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+app.use(express.json())
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.car_name}:${process.env.pass}@cluster0.6kfue.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    console.log('db connected')
+    // perform actions on the collection object
+    client.close();
+});
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
